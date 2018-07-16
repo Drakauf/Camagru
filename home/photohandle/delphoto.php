79a039feab81452da6e98271eff0a7af73b84f07
Shankithan Thevakumar <shthevak@z2r1p1.le-101.fr>
2018-07-16 11:52:14 +0200

    deleting photos with coms/likes

--- home/photohandle/delphoto.php
+++ home/photohandle/delphoto.php
@@ -5,6 +5,8 @@ else
 {
 	include '/var/www/html/functions/bdco.php';
 	$db = dataco();
+	$delcolik = $db->prepare("DELETE FROM Comlik WHERE image_id = ?");
+	$delcolik->execute([$_POST['id']]);
 	$delphoto = $db->prepare("DELETE FROM Image WHERE image_id = ?");
 	if(	$delphoto->execute([$_POST['id']]))
 		echo "done";
