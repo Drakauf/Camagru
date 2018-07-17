416df4a4e16633ece41be699d2ee6dcc7023e2e5
Shankithan Thevakumar <shthevak@z2r1p1.le-101.fr>
2018-07-17 14:53:36 +0200

    mail when photo is commented

--- home/photohandle/comment.php
+++ home/photohandle/comment.php
@@ -2,6 +2,19 @@
 session_start();
 include '/var/www/html/functions/bdco.php';
 
+function ft_mail($mail, $photo, $user)
+{
+	$sujet = "Une de vos photo vien d'etre commentee";
+	$entete = "From: shthevak@camagram.com";
+	$message = "Messagerie automatique de Camagru,
+
+		La photo $photo vien d'etre commentee par $user;
+
+	---------------
+		Ceci est un mail automatique, Merci de ne pas y répondre.";
+mail($mail, $sujet, $message, $entete);
+}
+
 if (isset($_POST['comment']))
 {
 	if (strlen($_POST['comment']) == 0)
@@ -13,7 +26,21 @@ if (isset($_POST['comment']))
 		$db = dataco();
 		$addcom = $db->prepare('INSERT INTO Comlik (type, image_id, user_id, comment) VALUES ( "C", ?, ?, ?);');
 		$addcom->execute([$_POST['id'], $_SESSION['User_id'], htmlspecialchars($_POST['comment'])]);
-		$getcom = $db->prepare('SELECT * FROM Comlik WHERE type LIKE "C" AND image_id = ?');
+
+		$getphoto = $db->prepare('SELECT * FROM Image WHERE image_id = ?');
+		$getphoto->execute([$_POST['id']]);
+		$photo = $getphoto->fetch(PDO::FETCH_ASSOC);
+
+		$getuser = $db->prepare('SELECT * FROM User WHERE user_id = ?');
+		$getuser->execute([$photo['user_id']]);
+		$user = $getuser->fetch(PDO::FETCH_ASSOC);
+
+		if ($user['notif'] == 1 && ($user['user_id'] != $_SESSION['User_id']))
+		{
+			ft_mail($user['mail'], $photo['image_name'], $_SESSION['User']);
+		}
+
+			$getcom = $db->prepare('SELECT * FROM Comlik WHERE type LIKE "C" AND image_id = ?');
 		$getcom->execute([$_POST['id']]);
 
 		if ($getcom->rowCount())
